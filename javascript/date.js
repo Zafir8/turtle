1e7ca4ebcf628915bc099d52b24791848345a20e
Zafir8 <61370398+Zafir8@users.noreply.github.com>
2023-08-26 21:23:39 +0530

    Date validation

---
+++ javascript/date.js
@@ -0,0 +1,13 @@
+function getToday() {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    if (month < 10) {
+        month = '0' + month;
+    }
+    let day = today.getDate();
+    if (day < 10) {
+        day = '0' + day;
+    }
+    return `${year}-${month}-${day}`;
+}
